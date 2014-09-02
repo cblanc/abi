fbe8358ddedf341480e77779358a151238c3e51f
cblanc <cablanchard@gmail.com>
2014-09-02 23:56:27 +0100

    Cleaning up Node example server

--- abi.go
+++ abi.go
@@ -26,15 +26,20 @@ func main() {
 	url := flag.String("url", "http://localhost:9999/", "URL to request")
 	n := flag.Int("n", 1000, "URL to request")
 	flag.Parse()
-	fmt.Println("Benchmarking: ", *url)
-	fmt.Println("Number of requests: ", *n)
+
+	fmt.Printf("Benchmarking: %s\n", *url)
+	fmt.Printf("Number of requests: %d\n\n", *n)
 
 	var times []float64
+	successfulResponses := 0
 
-	for i := 1; i < *n; i++ {
+	for i := 0; i < *n; i++ {
 		t := start()
-		request(*url)
-		times = append(times, end(t))
+		response, err := request(*url)
+		if err == nil && response.StatusCode == 200 {
+			successfulResponses++
+			times = append(times, end(t))
+		}
 	}
 
 	total := float64(0)
@@ -43,11 +48,13 @@ func main() {
 		total += time
 	}
 
-	average := total / float64(*n) / 1000000
+	average := total / float64(*n)
 
 	fmt.Println("Benchmark Complete")
-	fmt.Printf("Total Time: %.2f ms\n\n", total)
-	fmt.Printf("Average Time: %.2f ms\n\n", average)
+	fmt.Printf("Total Time: %.2f s\n", total/1000000000)
+	fmt.Printf("Average Time: %.2f ms\n", average/1000000)
+	fmt.Printf("Requests Succeeded: %.1f%%", 100*float64(successfulResponses)/float64(*n))
+	fmt.Printf("\n\n")
 
 	os.Exit(0)
 }
