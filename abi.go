a18189c7f8423da24fe7d5376f8f0af3cba26fac
cblanc <cablanchard@gmail.com>
2014-09-04 22:15:55 +0100

    Refactored

--- abi.go
+++ abi.go
@@ -3,8 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	// "github.com/olekukonko/tablewriter"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -22,39 +22,80 @@ func request(url string) (*http.Response, error) {
 	return response, err
 }
 
-func main() {
-	url := flag.String("url", "http://localhost:9999/", "URL to request")
-	n := flag.Int("n", 1000, "URL to request")
-	flag.Parse()
+type abiConfig struct {
+	url      string
+	requests int
+}
 
-	fmt.Printf("Benchmarking: %s\n", *url)
-	fmt.Printf("Number of requests: %d\n\n", *n)
+type abiResult struct {
+	totalTime           float64
+	averageTime         float64
+	successfulResponses int
+}
+
+func extractConfig() abiConfig {
+	var config abiConfig
+	config.url = *flag.String("url", "http://localhost:9999/", "URL to request")
+	config.requests = *flag.Int("n", 1000, "URL to request")
+	flag.Parse()
+	return config
+}
+
+// Return
+func digestResults(config abiConfig, times []float64, successfulResponses int) abiResult {
+	var result abiResult
+
+	total := float64(0)
+	for _, time := range times {
+		total += time
+	}
+	result.totalTime = total
+
+	result.averageTime = total / float64(config.requests)
+
+	result.successfulResponses = successfulResponses
+
+	return result
+}
+
+func toSeconds(time float64) float64 {
+	return time / 1000000000
+}
+
+func toMs(time float64) float64 {
+	return time / 1000000
+}
+
+func toPercent(num int, den int) float64 {
+	return 100 * float64(num) / float64(den)
+}
+
+func presentResults(result abiResult) {
+	fmt.Println("Benchmark Complete")
+	fmt.Printf("Total Time: %.2f s\n", toSeconds(result.totalTime))
+	fmt.Printf("Average Time: %.2f ms\n", toMs(result.averageTime))
+	// fmt.Printf("Requests Succeeded: %.1f%%", toPercent(result.successfulResponses, config.requests))
+	fmt.Printf("\n\n")
+}
+
+func main() {
+	config := extractConfig()
+
+	fmt.Printf("Benchmarking: %s\n", config.url)
+	fmt.Printf("Number of requests: %d\n\n", config.requests)
 
 	var times []float64
 	successfulResponses := 0
 
-	for i := 0; i < *n; i++ {
+	for i := 0; i < config.requests; i++ {
 		t := start()
-		response, err := request(*url)
+		response, err := request(config.url)
 		if err == nil && response.StatusCode == 200 {
 			successfulResponses++
 			times = append(times, end(t))
 		}
 	}
 
-	total := float64(0)
-
-	for _, time := range times {
-		total += time
-	}
-
-	average := total / float64(*n)
-
-	fmt.Println("Benchmark Complete")
-	fmt.Printf("Total Time: %.2f s\n", total/1000000000)
-	fmt.Printf("Average Time: %.2f ms\n", average/1000000)
-	fmt.Printf("Requests Succeeded: %.1f%%", 100*float64(successfulResponses)/float64(*n))
-	fmt.Printf("\n\n")
-
-	os.Exit(0)
+	result := digestResults(config, times, successfulResponses)
+	presentResults(result)
 }
